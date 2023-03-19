2ede38844600be3dfb640b40b25d63b49b4fec12
Fidgetman <a@mail.ru>
2023-03-19 23:52:47 +0300

    add changes solution task 12

--- dz_home_les_2/dz_task_12.py
+++ dz_home_les_2/dz_task_12.py
@@ -3,10 +3,27 @@ import math
 sum_numbers = int(input('введите сумму задуманных чисел: '))
 multiplication_numbers = int(input('введите произведение этих задуманных чисел: '))
 
-discriminant = sum_numbers*sum_numbers - 4 * multiplication_numbers
+first_term = 1
+second_term = sum_numbers - first_term
 
-fist_number = (sum_numbers - math.sqrt(discriminant)) / 2 
-print(fist_number)
+while(first_term * second_term != multiplication_numbers):
+    first_term += 1
+    second_term -= 1
+    if(first_term == sum_numbers):
+        break
 
-second_number = (sum_numbers + math.sqrt(discriminant)) / 2 
-print(second_number)
+if(second_term == 0):
+    discriminant = sum_numbers*sum_numbers - 4 * multiplication_numbers
+
+    fist_number = (sum_numbers - math.sqrt(discriminant)) / 2 
+    print(fist_number)
+
+    second_number = (sum_numbers + math.sqrt(discriminant)) / 2 
+    print(second_number)
+
+else:
+    print(first_term)
+    print(second_term)
+
+
+
